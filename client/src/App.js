35c61409bc16272a0e925b4d770ffd7a00442a8a
nelabv <nielleangelabv@gmail.com>
2021-11-26 09:24:32 +0800

    Added SASS

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
+import './App.scss'
 
 function App() {
   let history = useHistory();
@@ -12,20 +13,29 @@ function App() {
   }, [history])
 
   return (
-    <>
-      MediRecords 2021
-      <Link to="/users/login">
-        <button>Login for Users</button>
-      </Link>
+    <div>
+      <div>
+        <h1>MediRecords</h1>
+        <Link to="/users/login">
+          <button>Login for Users</button>
+        </Link>
 
-      <Link to="/physicians/login">
-        <button>Login for Physicians</button>
-      </Link>
+        <Link to="/physicians/login">
+          <button>Login for Physicians</button>
+        </Link>
 
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
-    </>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+
+      <div>
+        <h2>How it works</h2>
+
+        <p>An organized health record system is a mislooked problem in most third world countries which results to misdiagnosis.
+          MediRecords is a project that aims to provide an organized medical health record system.</p>
+      </div>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import { routes } from "./routes";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   routes,
