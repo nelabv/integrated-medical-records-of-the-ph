f8c92183565877a7e09ec5615a232e86bdd5bbf8
nelabv <nielleangelabv@gmail.com>
2022-02-02 16:03:58 +0800

    Styling for homepage added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
+import { FaNotesMedical, FaGithub } from 'react-icons/fa';
 import './App.scss';
 import NavHandler from './components/Nav/NavHandler';
+import Footer from './components/Footer';
 
 function App() {
   let history = useHistory();
@@ -18,27 +19,31 @@ function App() {
       <NavHandler />
 
       <div className="front-homepage">
-
           <h1>Integrated Medical Records of the Philippines</h1>
 
-          <div className='button-holder'>
-              <Link to="/login/as">
-                  <button className='primary-btn'>Login</button>  
-              </Link>
-              
-              <Link to="/register">
-                  <button className='secondary-btn'>Register</button>   
-              </Link>
-          </div>
+          <span>At vero eos et accusamus et iusto odio. Dignissimos ducimus qui blanditiis praesentium voluptatum</span>
 
-        </div>
-      
-      <div>
-
-        <h2>How it works</h2>
-
-        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+          <button className='primary-btn'>Get Started</button>
       </div>
+      
+      <div className='standard-cont'>
+            <div className='heading'>
+                <FaNotesMedical size="2em" />
+                <h2>How it works</h2>
+            </div>
+
+            <p className='block-text'>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+
+            <button className='primary-btn'>
+              <FaGithub size="1.5rem" className="button-icon"/>
+              View Github Repository
+            </button>
+      </div>
+
+      <div className='homepage-image-banner'>
+        <h3><span className='colored-text'>Quis nostrud exercitation</span> ullamco laboris nisi. </h3>
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+        <span className='all-caps'>INTEGRATED MEDICAL RECORDS OF THE PHILIPPINES 2021</span>
+    </footer>
+  );
+}
+
+export default Footer;
