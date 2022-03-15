66f07ef9e2fde2b5bf64f919cdc915dfc3c3b13a
nelabv <nielleangelabv@gmail.com>
2022-03-15 09:08:35 +0800

    Upload confirmation page styling

--- client/src/pages/UploadSuccess.js
+++ client/src/pages/UploadSuccess.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
+import NavHandler from "../components/Nav/NavHandler";
+import Footer from "../components/Footer";
 
 export default function UploadSuccess(props) {
   const data = props.history.location.state?.test;
@@ -13,8 +15,23 @@ export default function UploadSuccess(props) {
   }, [data, history])
   
   return (
-    <div>
-      <h1>UPLOAD SUCCESS</h1>
-    </div>
+    <>
+        <NavHandler />
+
+        <div className="gradient--blue">
+          <div className="upload-success--container">
+              <h2 className="teal--highlight">Uploaded file successfully</h2>
+              <p>Your selected file can now be accessed through the respective patient's directory. </p>
+
+
+      
+              <Link to='/'>
+                    <span>My Dashboard</span>
+              </Link>
+          </div>
+        </div>
+
+        <Footer />
+    </>
   );
 }
