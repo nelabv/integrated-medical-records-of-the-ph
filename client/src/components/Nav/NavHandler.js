f441dcf6b086a7ee4dc41486dd9965fe526d7c7f
nelabv <nielleangelabv@gmail.com>
2022-04-11 10:18:38 +0800

    Fixed logout button

--- client/src/components/Nav/NavHandler.js
+++ client/src/components/Nav/NavHandler.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import User from "../../methods/users";
 
 function NavHandler(props) {
   const { isNavTransparent } = props;
+  let history = useHistory();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    
+    User.logout()
+        .then((res, err) => {
+          if (err) throw err;
+
+          console.log(res)
+          localStorage.clear();
+          history.push("/dashboard")
+        })
+  }
 
   return (
     <>
@@ -10,7 +25,9 @@ function NavHandler(props) {
           <div className= {isNavTransparent ? "nav--transparent" : "nav"}>
                 <span>IRMP</span> 
 
-                { localStorage.getItem('ID') ? <button className="navbar--btn" >Logout</button> : 
+                { localStorage.getItem('ID') 
+                
+                    ? <button className="navbar--btn" onClick={handleLogout} >Logout</button> : 
                       <>
                           <Link to='/login/as'  className="navbar--btn">
                               Login
