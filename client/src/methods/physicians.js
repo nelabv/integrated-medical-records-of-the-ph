f16f3f26d54625bfa5c3a5818d31d6937f3bd2d5
nelabv <nielleangelabv@gmail.com>
2022-05-16 10:15:53 +0800

    Axios requests physicians portal fix

--- client/src/methods/physicians.js
+++ client/src/methods/physicians.js
@@ -16,34 +16,35 @@ const httpTESTING = axios.create({
   baseURL: URL,
   headers: {
     "Content-type": "multipart/form-data",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": true,
-    "Access-Control-Allow-Origin": "*"
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
   }
 });
 
 class Physician {
   login(form) {
-    return http.post("/physicians/login", form)
+    return http.post("/physicians/login", form, {withCredentials: true})
   }
 
   register(physicianInformation) {
-    return http.post("/physicians/register", physicianInformation)
+    return http.post("/physicians/register", physicianInformation, {withCredentials: true})
   }
 
   fetchPhysicianInformation() {
-    return http.get("/physicians/profile");
+    return http.get("/physicians/profile", {withCredentials: true});
   }
 
   generatePrescription(config) {
-    return http.post("/physicians/generate-prescription", config)
+    return http.post("/physicians/generate-prescription", config, {withCredentials: true})
   }
 
   uploadFileToPatientDatabase(formData, params) {
-    return httpTESTING.post(`/upload-to-bucket?id=${params.patientID}&type=${params.fileExtension}`, formData)
+    return httpTESTING.post(`/upload-to-bucket?id=${params.patientID}&type=${params.fileExtension}`, formData, {withCredentials: true})
   }
 
   verifyPatientInformation(query) {
-    return http.get(`/verify?id=${query.patientID}&last=${query.patientLastName}`)
+    return http.get(`/verify?id=${query.patientID}&last=${query.patientLastName}`, {withCredentials: true})
   }
 }
 
