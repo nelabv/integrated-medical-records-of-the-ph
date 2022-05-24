d8931a4b2ee04534d280347b038fedf29d19904b
nelabv <nielleangelabv@gmail.com>
2022-05-24 09:04:22 +0800

    Updated form reducer

--- client/src/reducers/FormReducer.js
+++ client/src/reducers/FormReducer.js
@@ -1,9 +1,18 @@
 const FormReducer = (state, action) => {
   switch (action.type) {
-    case 'ON CHANGE':
+    case "ON CHANGE":
       return {
         ...state,
         [action.field] : [action.payload]
+      }
+
+    case "UPDATE ADDRESS":
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          [action.field] : [action.payload]
+        }
       }
 
     default:
